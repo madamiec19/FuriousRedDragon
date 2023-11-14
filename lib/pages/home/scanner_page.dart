677acf554c5b4f080de0fe3f7412f9e1c75eca16
TranuszJacz <s217451@sggw.edu.pl>
2023-11-14 10:48:31 +0100

    Update scanner_page.dart

--- lib/pages/home/scanner_page.dart
+++ lib/pages/home/scanner_page.dart
@@ -8,37 +8,25 @@ class ScannerPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< Updated upstream
     return const MaterialApp(
-      DebugShowCheckedModeBanner: false;
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: kPageBackgroundColor,
         body: MainScannerPage(),
-=======
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: pageBackgroundColor,
-        body: MyWidget(),
->>>>>>> Stashed changes
       ),
     );
   }
 }
 
-<<<<<<< Updated upstream
 class MainScannerPage extends StatelessWidget {
   const MainScannerPage({super.key});
 
-=======
-class MyWidget extends StatelessWidget {
->>>>>>> Stashed changes
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-<<<<<<< Updated upstream
           Container(
             height: 200,
             width: 200,
@@ -54,41 +42,6 @@ class MyWidget extends StatelessWidget {
               );
             },
             buttonTitle: ('Wpisz kod ręcznie'),
-=======
-          // Obraz
-          Image.network(
-            'https://kropa.pl/data/gfx/pictures/large/7/4/110147_1.jpg', // Zastąp adresem URL obrazu
-            height: 200.0, // Dostosuj wysokość obrazu
-            width: 200.0, // Dostosuj szerokość obrazu
-          ),
-
-          SizedBox(
-              height: 20), // Pusty odstęp między obrazem a obrazem przycisku
-
-          // ElevatedButton jako przycisk
-          ElevatedButton(
-            onPressed: () {
-              // Przejście do nowej strony po naciśnięciu przycisku
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SecondPage()),
-              );
-            },
-            style: ElevatedButton.styleFrom(
-              primary: Colors.orange,
-              elevation: 5,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Wprowadź kod ręcznie',
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
->>>>>>> Stashed changes
           ),
         ],
       ),
@@ -96,34 +49,22 @@ class MyWidget extends StatelessWidget {
   }
 }
 
-<<<<<<< Updated upstream
 class SecondScannerPage extends StatelessWidget {
   const SecondScannerPage({super.key});
 
-=======
-class SecondPage extends StatelessWidget {
->>>>>>> Stashed changes
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-<<<<<<< Updated upstream
+      /*appBar: AppBar(
         title: const Text('Skanuj'),
         backgroundColor: kFuriousRedColor,
-      ),
+      ),*/
       body: Padding(
         padding: const EdgeInsets.all(16),
-=======
-        titleLarge: Text('Wprowadź kod kreskowy'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
->>>>>>> Stashed changes
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-<<<<<<< Updated upstream
             const Text(
               'Wprowadź kod kreskowy:',
               style: TextStyle(fontSize: 24),
@@ -136,26 +77,11 @@ class SecondPage extends StatelessWidget {
                     keyboardType: TextInputType.number,
                     decoration: InputDecoration(
                       hintText: 'Wprowadź cyfry',
-=======
-            Text(
-              'Wprowadź kod kreskowy:',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      hintText: 'Wprowadź kod',
->>>>>>> Stashed changes
                       border: OutlineInputBorder(),
                     ),
                   ),
                 ),
-<<<<<<< Updated upstream
-                SizedBox(width: 20),
+                const SizedBox(width: 20),
                 Image.network(
                   'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Minimalist_info_Icon.png/800px-Minimalist_info_Icon.png',
                   height: 50,
@@ -163,43 +89,12 @@ class SecondPage extends StatelessWidget {
                 ),
               ],
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             SmallButton(
               onTap: () {},
               buttonTitle: ('Zatwierdź'),
               backgroundColor: kDarkerGrey,
             )
-=======
-                SizedBox(width: 20),
-                Image.network(
-                  'https://edycja.pl/media/catalog/category/_jpII_www.png',
-                  height: 50,
-                  width: 50,
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Dodaj obsługę po naciśnięciu przycisku "Zatwierdź"
-                print('Zatwierdź');
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.all(8),
-                child: Text(
-                  'Zatwierdź',
-                  style: TextStyle(fontSize: 18),
-                ),
-              ),
-            ),
->>>>>>> Stashed changes
           ],
         ),
       ),
